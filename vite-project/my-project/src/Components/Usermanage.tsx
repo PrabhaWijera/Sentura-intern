cdbe32307951a405ca1217ab85b4df6a6dbf484d
PrabhaWijera <prabhashwijerathna2001@gmail.com>
2024-02-03 10:32:41 +0530

    add final

--- vite-project/my-project/src/Components/Usermanage.tsx
+++ vite-project/my-project/src/Components/Usermanage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+ import React, { useState, useEffect } from 'react';
 import { Weavy, WyChat } from "@weavy/uikit-web";
 import '@weavy/uikit-web';
 import img from "../assets/images.png";
